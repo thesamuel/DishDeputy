9f5779d532d183518911dc86550e0eb28d3a9d5d
Sam Gehman <sgehman@cs.uw.edu>
2017-05-10 15:51:09 -0700

    Added exit failure to main test file.

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
 
     if (!image.data) {
         printf("No image data \n");
-        return -1;
+        return EXIT_FAILURE;
     }
     namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", image);
